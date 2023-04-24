e6050216fe1477cd0239aedc2483a153496e8b7f
nooknock <wooh2013@gmail.com>
2023-04-24 12:20:06 +0900

    ㅋ

--- src/main/java/com/myboot03/member/dao/MemberDAO.java
+++ src/main/java/com/myboot03/member/dao/MemberDAO.java
@@ -14,7 +14,7 @@ public interface MemberDAO {
 
 	//그 전에 마이바티스에서는 DAO 인터페이스에 추상메소드를 선언한 후에 인터페이스 구현클래스에서 SqlSession 클래스로 매퍼 파일의 
 	//SQL문에 접근해서 실행 헀지만, 스프링 부트에서는 구현 클래스가 없어지고 서비스 클래스에서 DAO 인터페이스의 추상 메서드를 호출하면
-	//인터페이스에서는 매퍼 파일에서 호출된 이름과 동일한 id의 SQL문을 바로 사용할 수 있다.//
+	//인터페이스에서는 매퍼 파일에서 호출된 이름과 동일한 id의 SQL문을 바로 사용할 수 있다.//ㅎㄴ읻ㅈㄷ
 	public List selectAllMemberList() throws DataAccessException;
 
 	public int insertMember(MemberVO memberVO) throws DataAccessException;
